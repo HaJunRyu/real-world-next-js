434df6fd1a35fe31f96b5a24e5ed8c6e63b90b08
HaJunRyu <hjr4557@naver.com>
2022-01-18 00:00:00 +0900

    Initial commit
    Changes to be committed:
    modified:   next.config.js
    modified:   tsconfig.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['i.imgur.com'],
+  },
+  webpack(config) {
+    config.resolve.modules.push(__dirname);
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
