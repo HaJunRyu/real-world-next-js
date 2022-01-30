7722cc6d864ee871c59731c0a7301d69327e403e
HaJunRyu <hjr4557@naver.com>
2022-01-30 18:18:29 +0900

    Feat: 각각의 라우트를 상수로 정의
    Changes to be committed:
    new file:   src/consts/route.ts

---
+++ src/consts/route.ts
@@ -0,0 +1,7 @@
+export const HOME_PAGE = '/';
+export const ARTICLE_PAGE = '/article';
+export const EDIT_PAGE = '/editor';
+export const PROFILE_PAGE = '/profile';
+export const LOGIN_PAGE = '/login';
+export const REGISTER_PAGE = '/register';
+export const SETTINGS_PAGE = '/settings';
